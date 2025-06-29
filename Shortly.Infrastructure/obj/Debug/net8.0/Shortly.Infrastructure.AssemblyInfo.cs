e6e559955cc9e7d4235484d87ecee4f3c3d417d1
abdelrahmantaymour <abdalrahman.m.taymor@gmail.com>
2025-06-29 10:44:31 +0300

    Initial commit

--- Shortly.Infrastructure/obj/Debug/net8.0/Shortly.Infrastructure.AssemblyInfo.cs
+++ Shortly.Infrastructure/obj/Debug/net8.0/Shortly.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shortly.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8c023db63b8e754cebd969366116b1758f8f06b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shortly.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shortly.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
